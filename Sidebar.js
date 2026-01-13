63f660986a6da383b3c7baef013ce60354d4ac45
めだころ <153439300+medakoro0321@users.noreply.github.com>
2026-01-13 21:40:58 +0900

    add: サイドバーを追加

---
+++ Sidebar.js
@@ -0,0 +1,10 @@
+function stickySidebar()
+{
+    const sidebar = document.querySelector(".sidebar");
+    let windowTop = window.pageYOffset + 50;
+
+    sidebar.style.top = windowTop + "px";
+}
+
+window.addEventListener("scroll", stickySidebar);
+window.addEventListener("load", stickySidebar);
